313f1ec73674408e1dbee3f3281e3c04b5239ac4
john katua <johnkatua99@gmail.com>
2022-12-01 01:21:43 +0300

    added a form item

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -8,7 +8,25 @@ const LoginPage = () => {
         <div className='login--image'>
           <img src="/assets/img1.jpg" alt="login-image" />
         </div>
-        <div className='login--form'>Item2</div>
+        <div className='login--form'>
+          <h2>Login</h2>
+          <div className='login--form__item'>
+            <div className='form--item'>
+              <label>Email</label>
+              <input type={"email"} placeholder='Enter your email address' />
+            </div>
+            <div className='form--item'>
+              <label>Password</label>
+              <input type={"password"} placeholder='Enter your email address' />
+            </div>
+          </div>
+          <button>
+            Submit
+          </button>
+          <div>
+            I do not have an account yet...<span>Register Now</span>
+          </div>
+        </div>
       </div>
     </div>
   )
