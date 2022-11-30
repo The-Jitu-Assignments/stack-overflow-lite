25db3b91a1d419961fe93207b8d12d4fdad8f94a
john katua <johnkatua99@gmail.com>
2022-12-01 01:09:21 +0300

    added an image to the login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -5,7 +5,9 @@ const LoginPage = () => {
   return (
     <div className='login--page'>
       <div className='login--container'>
-        <div className='login--image'>Item1</div>
+        <div className='login--image'>
+          <img src="/assets/img1.jpg" alt="login-image" />
+        </div>
         <div className='login--form'>Item2</div>
       </div>
     </div>
