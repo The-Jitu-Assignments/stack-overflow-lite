f668951502821969148b23cd0fdb584bf79a0a59
john katua <johnkatua99@gmail.com>
2022-11-30 17:32:50 +0300

    user can navigate to different pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import { useNavigate } from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <div>Header</div>
+    <div>
+      <div onClick={() => navigate('/')}>Home</div>
+      <div onClick={() => navigate('/login')}>Login</div>
+      <div onClick={() => navigate('/register')}>Signup</div>
+    </div>
   )
 }
 
