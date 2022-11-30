4d8e012a4182cda66b8812a088a4aff89b8ccd64
john katua <johnkatua99@gmail.com>
2022-11-30 17:06:12 +0300

    add header component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
