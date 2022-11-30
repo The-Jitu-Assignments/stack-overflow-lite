5e36e4dfb05094adba416025b24138e560829ea4
john katua <johnkatua99@gmail.com>
2022-11-30 17:57:40 +0300

    user can navigate to different pages

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../Header/Header';
 import './layout.css'
 
 const Layout = ({ children }) => {
   return (
     <div className='layout'>
-      <div>Header</div>
+      <div>
+        <Header />
+      </div>
       <div className='layout--main'>
         {children}
       </div>
