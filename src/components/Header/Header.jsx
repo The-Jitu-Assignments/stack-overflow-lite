ba206ef75e8378daedf6e49647cd804f79c8f66b
john katua <johnkatua99@gmail.com>
2022-11-30 18:05:10 +0300

    style the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './header.css'
 
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className='header'>
       <div onClick={() => navigate('/')}>Home</div>
       <div onClick={() => navigate('/login')}>Login</div>
       <div onClick={() => navigate('/register')}>Signup</div>
