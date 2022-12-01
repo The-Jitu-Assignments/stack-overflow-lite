cba814e1c55f5efdb1db44fa1b9f7d059aead514
john katua <johnkatua99@gmail.com>
2022-12-01 13:45:32 +0300

    updated the sidebar modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,7 +7,26 @@ const Modal = ({ open, close }) => {
     <div className='modal--overlay'>
       <div className='modal--body'>
         <div className='modal--header'>
-          <div onClick={close}>Close</div>
+          <div onClick={close} className='modal--close__btn'>
+            x
+          </div>
+        </div>
+        <div className='modal--content'>
+          <div className="modal--content__item">
+            Home
+          </div>
+          <div className="modal--content__item">
+            All Questions
+          </div>
+          <div className="modal--content__item">
+            Unanswered Questions
+          </div>
+          <div className="modal--content__item">
+            About Us
+          </div>
+          <div className="modal--content__item">
+            Pricing
+          </div>
         </div>
       </div>
     </div>
