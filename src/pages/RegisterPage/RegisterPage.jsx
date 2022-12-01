197784967e2de4afd01ca4a661899c8428d310d3
john katua <johnkatua99@gmail.com>
2022-12-01 08:47:28 +0300

    styled the register page

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './register.css'
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
   return (
     <div className='registration--page'>
       <div className='register--container'>
-        <div className='register--form'>A</div>
+        <div className='register--form'>
+          <h2>Register</h2>
+          <div className='register--form__inputs'>
+            <div className='form--item'>
+              <label>Email:</label>
+              <input type={"email"} placeholder='Enter your email address' />
+            </div>
+            <div className='form--item'>
+              <label>Email:</label>
+              <input type={"email"} placeholder='Enter your email address' />
+            </div>
+            <div className='form--item'>
+              <label>Password:</label>
+              <input type={"password"} placeholder='Enter your email address' />
+            </div>
+          </div>
+          <button className='login--btn'>
+            Submit
+          </button>
+          <div className='login--text'>
+            Already have an account...<span onClick={() => navigate('/login')}>Register Now</span>
+          </div>
+        </div>
         <div className='register--image'>
           <img src="/assets/img1.jpg" alt='register-img' />
         </div>
