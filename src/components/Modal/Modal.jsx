4c4107ac52202812dbd2432ae6f4ead9ce915495
john katua <johnkatua99@gmail.com>
2022-12-01 14:12:47 +0300

    added background color to modal items

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './modal.css'
+import './modal.css';
+import { AiFillHome,  } from 'react-icons/ai';
+import { GiPapers, GiPriceTag } from 'react-icons/gi'
+import { BsQuestionSquareFill } from 'react-icons/bs';
+import { GrLanguage } from 'react-icons/gr';
 
 const Modal = ({ open, close }) => {
   if (!open) return null;
   return (
-    <div className='modal--overlay'>
-      <div className='modal--body'>
+    <div className='modal--overlay' onClick={close}>
+      <div className='modal--body' onClick={(e) => e.stopPropagation()}>
         <div className='modal--header'>
           <div onClick={close} className='modal--close__btn'>
             x
@@ -13,18 +17,23 @@ const Modal = ({ open, close }) => {
         </div>
         <div className='modal--content'>
           <div className="modal--content__item">
+            <AiFillHome />
             Home
           </div>
           <div className="modal--content__item">
+            <GiPapers />
             All Questions
           </div>
           <div className="modal--content__item">
+            <BsQuestionSquareFill />
             Unanswered Questions
           </div>
           <div className="modal--content__item">
+            <GrLanguage />
             About Us
           </div>
           <div className="modal--content__item">
+            <GiPriceTag />
             Pricing
           </div>
         </div>
