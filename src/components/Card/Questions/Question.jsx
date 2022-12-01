1d5e6f0c1bfb309cc92f8e706b7bd306232d87f9
john katua <johnkatua99@gmail.com>
2022-12-01 19:54:18 +0300

    added user image to the question card

--- src/components/Card/Questions/Question.jsx
+++ src/components/Card/Questions/Question.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './question.card.css'
 
-const QuestionCard = () => {
+const QuestionCard = ({ post }) => {
   return (
     <div className='question--card'>
-      <div className='question--card__header'>Header</div>
+      <div className='question--card__header'>
+        <div className='question--user__img'>
+          <img src={post.image} alt="user--face" />
+        </div>
+        <div className='question--user__details'>Data</div>
+        <div className='question--user__links'>Links</div>
+      </div>
       <div className='question--card__body'>Body</div>
       <div className='question--card__footer'>Footer</div>
     </div>
