0414dce1002b11876b36cdda664cd5ab36cb2d30
john katua <johnkatua99@gmail.com>
2022-12-02 18:56:31 +0300

    added user modal header and body

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,7 +3,10 @@ import './user.css'
 
 const User = ({ close }) => {
   return (
-    <div className='user--modal' onClick={close}>User</div>
+    <div className='user--modal' onClick={close}>
+      <div className='user--modal__header'>Header</div>
+      <div className='user--modal__content'>Body</div>
+    </div>
   )
 }
 
