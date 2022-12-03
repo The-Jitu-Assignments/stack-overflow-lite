202267bf89ab8fc77deffc7745ec5645d3d72d66
john katua <johnkatua99@gmail.com>
2022-12-03 22:32:45 +0300

    added an answer body

--- src/components/ViewAnswers/ViewAnswers.jsx
+++ src/components/ViewAnswers/ViewAnswers.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './viewanswer.css'
+import Button from '../Button/Button'
 
 const ViewAnswers = ({ close }) => {
   return (
     <div className='viewAnswer--modal' onClick={close}>
-      <div className='viewAnswer--header'>Header</div>
+      <div className='viewAnswer--header'>
+        <h2>Answers</h2>
+        <Button method={close} className={"closeBtn"} text={"x"} />
+      </div>
+      <div className='viewAnswer--body'>
+        <div className="test--item">Item1</div>
+        <div className="test--item">Item2</div>
+        <div className="test--item">Item3</div>
+        <div className="test--item">Item4</div>
+        <div className="test--item">Item5</div>
+        <div className="test--item">Item6</div>
+        <div className="test--item">Item7</div>
+        <div className="test--item">Item8</div>
+        <div className="test--item">Item9</div>
+        <div className="test--item">Item10</div>
+      </div>
     </div>                                                                                                                                                                            
   )
 }
