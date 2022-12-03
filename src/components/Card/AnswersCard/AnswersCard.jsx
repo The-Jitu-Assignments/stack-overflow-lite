fbed8d2d6421c2ebc530b2973d009f9dfc307cef
john katua <johnkatua99@gmail.com>
2022-12-03 23:15:06 +0300

    added view answer header

--- src/components/Card/AnswersCard/AnswersCard.jsx
+++ src/components/Card/AnswersCard/AnswersCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Button from '../../Button/Button'
 import './answersCard.css'
 
 const AnswersCard = ({ post }) => {
   return (
     <div className='answersCard'>
-      <div>Header</div>
+      <div className='answersCard__header'>
+        <div className='answersCard__header--user'>   
+          <img src={post.image} alt="user-img" />
+        </div>
+        <Button className={"accept--btn"} text={"Accept"} />
+      </div>
       <div>Body</div>
       <div>Footer</div>
     </div>
