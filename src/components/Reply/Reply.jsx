24b390a2a158ffca60af347b24af48a526d75fb4
john katua <johnkatua99@gmail.com>
2022-12-04 00:03:06 +0300

    added gap to the rerply component

--- src/components/Reply/Reply.jsx
+++ src/components/Reply/Reply.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from '../Button/Button';
 import './reply.css';
 
 const Reply = ({ close }) => {
   return (
-    <div className='reply--modal' onClick={close}>Reply</div>
+    <div className='reply--modal' onClick={close}>
+      <div>Header</div>
+      <textarea></textarea>
+      <Button method={close} className={'submitBtn'} text={"Submit"} />
+    </div>
   )
 }
 
