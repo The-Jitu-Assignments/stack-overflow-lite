9f76abec7a4fc76da2c738f70c3cc15bdb887a59
john katua <johnkatua99@gmail.com>
2022-12-04 10:29:15 +0300

    added profile details

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './Profile.css'
+import dummyData from '../../data/posts.json';
 
 const Profile = () => {
+  const { posts } = dummyData;
+  const img = posts.map((post) => post.image);
   return (
     <div className='profile--page'>
       <div className='profile--page__left'>
-        <div className='profile--image'>Image</div>
-        <div>Name</div>
-        <div>Email</div>
+        <div className='profile--image'>
+          <img src={img} alt="user--img" />
+        </div>
+        <div>John Katua</div>
+        <div>johnkatua99@gmail.com</div>
       </div>
-      <div className='profile--page__right'>Other Details</div>
-      {/* <div className='profile--page__intro'>
-        <img src='/assets/img2.jpg' alt='logo' />
+      <div className='profile--page__right'>
+        <div className='profile--right__top'>
+          Other details
+        </div>
+        <div className='profile--right__bottom'>
+          Bottom
+        </div>
       </div>
-      <div className='profile--page__content'>
-        Content
-      </div> */}
     </div>
   )
 }
