8eebc691733bc949ab96be6a78e2367f8abf7f1b
john katua <johnkatua99@gmail.com>
2022-12-04 09:57:15 +0300

    added a profile page

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Profile.css'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile--page'>
+      <div className='profile--page__left'>
+        <div className='profile--image'>Image</div>
+        <div>Name</div>
+        <div>Email</div>
+      </div>
+      <div className='profile--page__right'>Other Details</div>
+      {/* <div className='profile--page__intro'>
+        <img src='/assets/img2.jpg' alt='logo' />
+      </div>
+      <div className='profile--page__content'>
+        Content
+      </div> */}
+    </div>
   )
 }
 
