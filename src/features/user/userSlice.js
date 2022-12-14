5c0e996a9f9cb930581627e117dbdb9f53dba740
john katua <johnkatua99@gmail.com>
2022-12-14 22:28:01 +0300

    added fullfilled and rejected cases to the user slice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from  '@reduxjs/toolkit';
-import { fetchUserProfile, registerUser } from './userActions';
+import { fetchUserProfile, registerUser, loginUser } from './userActions';
 
 const initialState = {
   user: null,
@@ -22,11 +22,20 @@ const userSlice = createSlice({
     builder.addCase(fetchUserProfile.fulfilled, (state, action) => {
       state.profile = action.payload
     });
+    // register
     builder.addCase(registerUser.fulfilled, (state, action) => {
       state.user = action.payload
       state.errorMessage = ''
     });
     builder.addCase(registerUser.rejected, (state, action) => {
+      state.errorMessage = action.payload
+    });
+
+    // login 
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.user = action.payload
+    });
+    builder.addCase(loginUser.rejected, (state, action) => {
       state.errorMessage = action.payload
     })
   }
