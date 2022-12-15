bdd50acf3af5b2a2888f37e98fa593f575c8ed33
john katua <johnkatua99@gmail.com>
2022-12-15 14:55:31 +0300

    added a state capture the question asked

--- src/components/AddQuestion/AddQuestion.jsx
+++ src/components/AddQuestion/AddQuestion.jsx
@@ -2,19 +2,38 @@ import React, { useState } from 'react';
 import Button from '../Button/Button';
 import './addquestion.css'
 import { ClipLoader } from 'react-spinners';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addAQuestion } from '../../features/question/quizActions';
 
 const AddQuestion = ({ close }) => {
   const { isLoading } = useSelector(state => state.quiz);
   const [question, setQuestion] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    dispatch(addAQuestion(question));
+    setQuestion('');
+    if (!isLoading) {
+      close();
+    }
+  }
   return (
     <div onClick={(e) => e.stopPropagation()} className='addquestion--modal'>
       <div className='addquestion--modal__header'>
         <h2>Post a Question</h2>
         <Button method={close} className={"closeBtn"} text={"X"} />
       </div>
-      <textarea placeholder='Enter your question and make it descriptive...'></textarea>
-      <Button method={() => setIsLoading(!loading)} className={"submitButton"} text={isLoading ? <ClipLoader color='white' size={20} /> : 'Submit'} />
+      <textarea 
+        placeholder='Enter your question and make it descriptive...'
+        name='question'
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+      ></textarea>
+      <Button 
+        method={handleSubmit} 
+        className={"submitButton"} 
+        text={isLoading ? <ClipLoader color='white' size={20} /> : 'Submit'} 
+      />
     </div>
   )
 }
