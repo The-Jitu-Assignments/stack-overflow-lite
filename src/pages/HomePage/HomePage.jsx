92815dadd92323c2cca8771e6521dd3f184f59da
john katua <johnkatua99@gmail.com>
2022-12-17 12:44:20 +0300

    cleaned up the homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchQuestions, setSelectedQuiz } from '../../features/question/quizSlice';
+import { fetchQuestions } from '../../features/question/quizSlice';
 import './home.css';
-import dummyData from '../../data/posts.json';
 import QuestionCard from '../../components/Card/Questions/Question';
 import { BsFilter } from 'react-icons/bs'
 import Button from '../../components/Button/Button';
-import AnswersCard from '../../components/Card/AnswersCard/AnswersCard';
 import { getQuestion } from '../../features/question/quizSlice';
 import { getLoggedInUser } from '../../features/user/userActions';
 
 const HomePage = () => {
-  const { selectedQuiz, questions } = useSelector(state => state.quiz);
-  const { user } = useSelector(state => state.user);
-  console.log(user)
-  const { answers, question } = selectedQuiz || [];
+  const { questions } = useSelector(state => state.quiz);
   const dispatch = useDispatch();
   const [showFilterBtns, setShowFilterBtns] = React.useState(false);
 
@@ -54,20 +49,8 @@ const HomePage = () => {
               )
             })}
 
-          </div>
-          
+          </div>    
         </div>
-        {/* <div className='homepage--premium'>
-          <h2>Answers</h2>
-          <h3>{question?.question}</h3>
-          {answers?.length > 0 ? (
-            <>
-              {answers?.map(answer => (
-                <AnswersCard key={answer.id} answer={answer} />
-              ))}
-            </>
-          ) : (selectedQuiz && <span>This question is not answered yet...</span>)}
-        </div> */}
       </div>
     </div>
   )
