d898ae601289a14d2763865cd8cf4f71c2021dfc
john katua <johnkatua99@gmail.com>
2022-12-17 10:02:02 +0300

    display the avatar of the user who added a comment

--- src/components/Card/Comments/Comment.jsx
+++ src/components/Card/Comments/Comment.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
+import { createAvatar } from '../../../helpers/avatar/CreateAvatar'
 import './comment.css'
 
-const Comment = () => {
+const Comment = ({ comment }) => {
+  const avatar = createAvatar(comment.name);
   return (
-    <>
     <div className='comment--box'>
-      Sample comment...
+      <div className='comment--header'>
+        <div className='comment--header__img'>
+          {comment.img ? (
+            <img src={comment.imgUrl} alt='comment--img' className='comment--img' /> 
+          ) : (<div className='comment--avatar'>{avatar}</div>)}
+        </div>
+        <div></div>
+      </div>
+      <div>{comment.comment}</div>
     </div>
-    <div className='comment--box'>
-      Sample comment...
-    </div>
-    <div className='comment--box'>
-      Sample comment...
-    </div>
-    </>
   )
 }
 
